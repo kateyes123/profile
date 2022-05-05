7a6d865e6d5cdfe5903c71154508968dde87b98f
kateyes123 <kateyes123@daum.net>
2022-05-05 18:19:37 +0900

    다듬기

--- startbootstrap-creative-gh-pages/js/scripts.js
+++ startbootstrap-creative-gh-pages/js/scripts.js
@@ -7,68 +7,68 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+//     // Navbar shrink function
+//     var navbarShrink = function () {
+//         const navbarCollapsible = document.body.querySelector('#mainNav');
+//         if (!navbarCollapsible) {
+//             return;
+//         }
+//         if (window.scrollY === 0) {
+//             navbarCollapsible.classList.remove('navbar-shrink')
+//         } else {
+//             navbarCollapsible.classList.add('navbar-shrink')
+//         }
 
-    };
+//     };
 
     
-      // Get the modal
-      var modal = document.getElementById('id01');
+//       // Get the modal
+//       var modal = document.getElementById('id01');
       
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-          if (event.target == modal) {
-              modal.style.display = "none";
-          }
-      }
+//       // When the user clicks anywhere outside of the modal, close it
+//       window.onclick = function(event) {
+//           if (event.target == modal) {
+//               modal.style.display = "none";
+//           }
+//       }
     
 
-    // Shrink the navbar 
-    navbarShrink();
+//     // Shrink the navbar 
+//     navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+//     // Shrink the navbar when page is scrolled
+//     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+//     // Activate Bootstrap scrollspy on the main nav element
+//     const mainNav = document.body.querySelector('#mainNav');
+//     if (mainNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#mainNav',
+//             offset: 74,
+//         });
+//     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+//     // Activate SimpleLightbox plugin for portfolio items
+//     new SimpleLightbox({
+//         elements: '#portfolio a.portfolio-box'
+//     });
 
-});
+// });
 
   // 회원가입 
   $('.form').find('input, textarea').on('keyup blur focus', function (e) {
@@ -162,6 +162,7 @@ window.addEventListener('load', function() {
 $(document).ready(function(){
     var zindex = 10;
     
+    console.log('!')
     $("div.card").click(function(e){
       e.preventDefault();
   
